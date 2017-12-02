2131baa086d97903d98777ef10e173e3568ef7e2
N Fourtounis <nickfortune@windowslive.com>
2017-12-02 21:52:47 +0200

    classes

--- src/java_practice/Room.java
+++ src/java_practice/Room.java
@@ -3,10 +3,20 @@ package java_practice;
  *
  * @author NFourtounis
  */
-public class Room {
+public class Room implements Rented{
     private String ID;
     private int capacity;
     private boolean view;
     private String type;
+
+    @Override
+    public boolean isAvailable() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int calculateCost() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/java_practice/Vehicle.java
+++ src/java_practice/Vehicle.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package java_practice;
-
 /**
  *
  * @author NFourtounis
  */
 public class Vehicle {
+    private String ID;
+    private String type;
     
 }
